29fb5136141ca7f6a9028b4cad5d4c54a3448b74
eduardom <edo.martinez01@gmail.com>
2019-11-11 00:15:41 +0100

    router use fix

--- zadanie/src/app/code/signup/signup.component.ts
+++ zadanie/src/app/code/signup/signup.component.ts
@@ -25,7 +25,9 @@ export class SignupComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.authServiceRegisterSub.unsubscribe();
+    if (this.authServiceRegisterSub){
+      this.authServiceRegisterSub.unsubscribe();
+    }
   }
 
   checkEqualPasswords(): boolean {
